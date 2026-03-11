ff44c24365f4d5f3c19bf111683dd6e656dfc3ee
Alex Maxeven <ax.zaverukha@gmail.com>
2026-03-11 20:48:55 +0200

    add style to nav menu

--- src/shared/ui/PageToc/PageToc.jsx
+++ src/shared/ui/PageToc/PageToc.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import styles from "./PageToc.module.css";
 
-const PageToc = ({ title = "На цій сторінці", items = [] }) => {
+const PageToc = ({ title = "Навігація на цій сторінці", items = [] }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
